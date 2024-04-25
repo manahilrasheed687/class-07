1e4b3e2755a787cc0eadc5fb47eb192d7a5dc038
Your Name <manahilrasheed507@gmail.com>
2024-04-25 04:46:58 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env
+let cond_1 = true;
+let cond_2 = false;
+if (cond_1 || cond_2) {
+    console.log("your grade is A+");
+}
+else {
+    console.log("your grade is A");
+}
+export {};
